b5b8e6d2a6edbc7e4a9fbd76b160718aad1d2394
Jeff Bai <j.bai@wustl.edu>
2021-01-03 06:11:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  attendees.each do |attendee|
+    puts badge_maker(attendee)
+    puts assign_rooms(attendee)
+  end
 end
